9c5a2945a4045028b6719dc844c1481736bb576d
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-01-06 15:52:27 -0300

    update

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.use(bodyParser.json())
 
 const mensagens = ['Essa é a primeira mensagem', 'Essa é a segunda mensagem']
 
-//[GET] / mensagens - Retorna a lista de mensagens
+//[GET] /mensagens - Retorna a lista de mensagens
 app.get('/mensagens', (req, res) => {
   res.send(mensagens)
 })
 
-//[GET] / mensagens/{id} - Retorna apenas uma unica mensagem por ID
+//[GET] /mensagens/{id} - Retorna apenas uma unica mensagem por ID
 app.get('/mensagens/:id', (req, res) => {
   const id = req.params.id
   const mensagem = mensagens[id]
@@ -27,6 +27,17 @@ app.post('/mensagens', (req, res) => {
   //  console.log(mensagem)
   mensagens.push(mensagem)
   res.send(`Mensagem criada com sucesso: '${mensagem}'.`)
+})
+
+//[PUT] /mensagens/{id} - Atualiza uma mensagem pelo ID
+app.put('/mensagens/:id', (req, res) => {
+  const id = req.params.id
+
+  const mensagem = req.body.mensagem
+
+  mensagens[id] = mensagem
+
+  res.send(`Mensagem atualizada com sucesso: '${mensagem}'.`)
 })
 
 app.listen(port, () => {
