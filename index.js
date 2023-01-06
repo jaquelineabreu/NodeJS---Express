d5a3a38f716e82d0898fd225bb268f9910fcc191
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-01-06 15:05:36 -0300

    add get/id

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const port = 3000
 
 const mensagens = ['Essa é a primeira mensagem', 'Essa é a segunda mensagem']
 
+//[GET] / mensagens - Retorna a lista de mensagens
 app.get('/mensagens', (req, res) => {
   res.send(mensagens)
+})
+
+//[GET] / mensagens/{id} - Retorna apenas uma unica mensagem por ID
+app.get('/mensagens/:id', (req, res) => {
+  const id = req.params.id
+  const mensagem = mensagens[id]
+  res.send(mensagem)
 })
 
 app.listen(port, () => {
